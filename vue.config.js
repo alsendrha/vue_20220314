7dc67b47c933a922076c63297cb2d52d17f6f126
daheeka <alsendrha@naver.com>
2022-03-14 14:17:34 +0900

    20220314
    dd

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// CORS => 같은 서버가 아니면 연동이 안됨
+// proxy를 설정하면 해결됨, 단, android등은 CORS를 반드시 설정해야 함
+
+module.exports = {
+    devServer:{
+        proxy : {
+            '/member' : {
+                target:'http://localhost:3000',
+                changeOrigin :true,
+                logLevel : 'debug'
+            },
+        },
+    }
+}
