41b54b4f7fa9021c7241e69e010e8430fe5a456e
daheeka <alsendrha@naver.com>
2022-03-16 17:39:50 +0900

    20220316
    오늘꺼

--- src/components/mypage/Menu2.vue
+++ src/components/mypage/Menu2.vue
@@ -2,62 +2,86 @@
     <div>
         <h3>암호변경</h3>
         <el-card>
-            <el-form :inline="true"  >
-                <el-form-item  label="기존암호" label-width="80px">
-                    <el-input  size="mini" type="password" v-model="state.pw" placeholder="기존암호" />
-                </el-form-item>
-            </el-form>
-            <el-form :inline="true"  >
-                <el-form-item  label="새암호" label-width="80px" style="margin-top:-20px">
-                    <el-input  size="mini" type="password" v-model="state.pw1"  placeholder="새암호" />
-                </el-form-item>
-            </el-form>
-            <el-form :inline="true"  >
-                <el-form-item  label="암호확인" label-width="80px" style="margin-top:-20px">
-                    <el-input  size="mini" type="password" v-model="state.pw2"  placeholder="암호확인" />
-                </el-form-item>
-            </el-form>
-            <el-button type="primary" style="margin-left:80px" size="mini" round @click="handleUpdate" >수정하기</el-button>
+                <el-form :inline="true"  >
+                    <el-form-item  label="기존암호" label-width="80px">
+                        <el-input  size="mini" type="password" ref="pw" v-model="state.pw" placeholder="기존암호" />
+                    </el-form-item>
+                </el-form>
+                <el-form :inline="true"  >
+                    <el-form-item  label="새암호" label-width="80px" style="margin-top:-20px">
+                        <el-input  size="mini" type="password" ref="newpw" v-model="state.newpw"  placeholder="새암호" />
+                    </el-form-item>
+                </el-form>
+                <el-form :inline="true"  >
+                    <el-form-item  label="암호확인" label-width="80px" style="margin-top:-20px">
+                        <el-input  size="mini" type="password" ref="newpw1" v-model="state.newpw1"  placeholder="암호확인" />
+                    </el-form-item>
+                </el-form>
+                <el-button type="primary" style="margin-left:80px" size="mini" round @click="handleUpdate" >수정하기</el-button>
         </el-card>
     </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive, ref} from 'vue'
 import axios from 'axios';
 export default {
     setup () {
+
         const state = reactive({
+            pw : '',
+            newpw : '',
+            newpw1 : '',
             token : sessionStorage.getItem("TOKEN")
 
         });
 
+        const pw = ref(null);
+        const newpw = ref(null);
+        const newpw1 = ref(null);
+
         const handleUpdate = async() => {
+            if(state.pw.length <= 0){
+                alert('암호를 입력하세요');
+                pw.value.focus();
+                return false;
+            }
+
+            if(state.newpw.length <= 0){
+                alert('새암호를 입력하세요');
+                newpw.value.focus();
+                return false;
+            }
+            if(state.newpw1.length <= 0){
+                alert('암호확인을 입력하세요');
+                newpw1.value.focus();
+                return false;
+            }
+            if(state.newpw1 !== state.newpw){
+                alert('암호가 일치하지 않습니다.');
+                newpw1.value.focus();
+                return false;
+            }
+
             const url = `/member/updatepw`;
-            const headers = {"Content=Type":"application/json", "auth":state.token};
+            const headers = {"Content-Type":"application/json", "auth":state.token};
             const body = {
                 pw : state.pw,
-                pw1 : state.pw1
+                newpw : state.newpw
             }
             const response = await axios.put(url, body, {headers});
             console.log(response.data);
             if(response.data.status===200){
                 alert('수정되었습니다.');
+                state.pw = '';
+                state.newpw = '';
+                state.newpw1 = '';
             }
         }
 
-        // onMounted(() => {
-        //     handleData();
-        // })
 
-        // const handleData = async() => {
-        //     const url = `/member/selectonepw`
-        //     const headers = {"Content-Type":"application/json", "auth":state.token};
 
-        // }
-        
-
-        return {state, handleUpdate}
+        return {state, pw, newpw, newpw1, handleUpdate}
     }
 }
 </script>
